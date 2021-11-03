c48732ebf84f3f7ac69db7aae3dfdd4c26e179bb
Manuel Vögele <develop@manuel-voegele.de>
2021-11-03 15:30:11 +0100

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,45 @@
+import { libWrapper } from "./libwrapper_shim.js";
+
+let ignoreVisionToggle;
+
+Hooks.once("init", () => {
+	window.ignoreVision = false;
+	libWrapper.register("ignore-vision", "SightLayer.prototype.tokenVision", tokenVision, "MIXED");
+});
+
+Hooks.on("getSceneControlButtons", controls => {
+	if (!ignoreVisionToggle) {
+		ignoreVisionToggle = {
+			name: "ignoreVision",
+			title: "Ignore token vision",
+			icon: "far fa-eye-slash",
+			toggle: true,
+			active: ignoreVision,
+			onClick: handleToggle,
+			visible: game.user.isGM,
+		};
+	}
+	const tokenControls = controls.find(group => group.name === "token").tools;
+	tokenControls.push(ignoreVisionToggle);
+});
+
+function handleToggle(toggled) {
+	ignoreVision = toggled;
+	canvas.perception.schedule({
+		sight: {
+			initialize: true,
+			refresh: true,
+			forceUpdateFog: true,
+		},
+		lighting: { refresh: true },
+		sounds: { refresh: true },
+		foreground: { refresh: true },
+	});
+}
+
+function tokenVision(wrapped) {
+	if (ignoreVision && game.user.isGM) {
+		return false;
+	}
+	return wrapped();
+}
